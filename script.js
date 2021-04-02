f0903681dcc5a0b2cef00038c9348eba6d105e92
trandrew <19401994+trandrew96@users.noreply.github.com>
2021-04-02 16:26:07 -0700

    make buttons invisible until slider is hovered on. Fix moveLeft button

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const btnRight = document.getElementById("moveRight");
 const indicators = document.querySelectorAll(".indicator");
 
 let baseSliderWidth = slider.offsetWidth;
-let activeIndex = 0;
+let activeIndex = 0; // the current page on the slider
 
 let movies = [
   {
@@ -13,90 +13,74 @@ let movies = [
   },
   {
     src:
-      "https://images.unsplash.com/photo-1611419010196-a360856fc42f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
-    title: "Toy Story",
+      "https://images.unsplash.com/photo-1579566346927-c68383817a25?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
   },
 
   {
     src:
       "https://images.unsplash.com/photo-1594736797933-d0501ba2fe65?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=674&q=80",
-    title: "Toy Story",
   },
-  {
-    src:
-      "https://images.unsplash.com/photo-1579566346927-c68383817a25?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-    title: "Toy Story",
-  },
+
   {
     src:
       "https://images.unsplash.com/photo-1617182635496-c5c474367085?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-    title: "Toy Story",
+  },
+  {
+    src:
+      "https://images.unsplash.com/photo-1611419010196-a360856fc42f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
   },
   {
     src:
       "https://images.unsplash.com/photo-1528495612343-9ca9f4a4de28?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80",
-    title: "Toy Story",
   },
   {
     src:
       "https://images.unsplash.com/photo-1518715303843-586e350765b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-    title: "Toy Story",
   },
   {
     src:
       "https://images.unsplash.com/photo-1617258683488-df59909f25f0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=644&q=80",
-    title: "Toy Story",
   },
   {
     src:
       "https://images.unsplash.com/photo-1543862809-2c9e0bcdc075?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=564&q=80",
-    title: "Toy Story",
   },
   {
     src:
       "https://images.unsplash.com/photo-1579156412503-f22426cc6386?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1352&q=80",
-    title: "Toy Story",
   },
   {
     src:
       "https://images.unsplash.com/photo-1514068574489-503a8eb91592?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1390&q=80",
-    title: "Toy Story",
   },
   {
     src:
       "https://images.unsplash.com/photo-1521714161819-15534968fc5f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-    title: "Toy Story",
   },
   {
     src:
       "https://images.unsplash.com/photo-1572188863110-46d457c9234d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-    title: "Toy Story",
   },
   {
     src:
       "https://images.unsplash.com/photo-1579702455224-c0dd4ac78234?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1369&q=80",
-    title: "Toy Story",
   },
 
   {
     src:
       "https://images.unsplash.com/photo-1575470180257-7183ddca844f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80",
-    title: "Toy Story",
   },
   {
     src:
       "https://images.unsplash.com/photo-1584253660192-de72b033c220?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
-    title: "Toy Story",
   },
   {
     src:
       "https://images.unsplash.com/photo-1611523792722-16952e48cffa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
-    title: "Toy Story",
   },
   {
     src:
       "https://images.unsplash.com/photo-1536300007881-7e482242baa5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-    title: "Toy Story",
   },
 ];
 
@@ -119,6 +103,7 @@ populateSlider();
 const initialMovie = document.getElementById("movie0");
 initialMovie.remove();
 
+// Update the indicators that show which page we're currently on
 function updateIndicators(index) {
   indicators.forEach((indicator) => {
     indicator.classList.remove("active");
@@ -127,34 +112,29 @@ function updateIndicators(index) {
   newActiveIndicator.classList.add("active");
 }
 
-slider.addEventListener("scroll", (e) => {
-  let x = slider.scrollLeft;
-});
-
 // Scroll Left button
 btnLeft.addEventListener("click", (e) => {
-  console.log("going left");
-  let width = slider.offsetWidth;
+  let movieWidth = document.querySelector(".movie").offsetWidth;
+  let scrollDistance = movieWidth * 6; // Scroll the length of 6 movies. TODO: make work for mobile because (4 movies/page instead of 6)
+
   slider.scrollBy({
     top: 0,
-    left: -width,
+    left: -scrollDistance,
     behavior: "smooth",
   });
+  activeIndex = (activeIndex - 1) % 3;
+  console.log(activeIndex);
+  updateIndicators(activeIndex);
 });
 
-// Sroll Right button
+// Scroll Right button
 btnRight.addEventListener("click", (e) => {
-  console.log("going right");
-
   let movieWidth = document.querySelector(".movie").offsetWidth;
-  let scrollDistance = movieWidth * 6;
-  let x = slider.scrollLeft;
+  let scrollDistance = movieWidth * 6; // Scroll the length of 6 movies. TODO: make work for mobile because (4 movies/page instead of 6)
 
-  console.log(`x = ${x}`);
-  console.log(`scrollDistance = ${scrollDistance}`);
-
+  // if we're on the last page
   if (activeIndex == 2) {
-    // duplicate all the items in the slider
+    // duplicate all the items in the slider (this is how we make 'looping' slider)
     populateSlider();
     slider.scrollBy({
       top: 0,
@@ -163,13 +143,6 @@ btnRight.addEventListener("click", (e) => {
     });
     activeIndex = 0;
     updateIndicators(activeIndex);
-
-    // Scroll back to beginning if we're currently at the end
-    // slider.scrollBy({
-    //   top: 0,
-    //   left: -9999,
-    //   behavior: "smooth",
-    // });
   } else {
     slider.scrollBy({
       top: 0,
@@ -180,15 +153,9 @@ btnRight.addEventListener("click", (e) => {
     console.log(activeIndex);
     updateIndicators(activeIndex);
   }
-
-  x = slider.scrollLeft;
-  width = slider.offsetWidth;
-
-  console.log(`x = ${x}`);
-  console.log(`width = ${width}`);
 });
 
-slider.addEventListener("scroll", (e) => {
-  console.log(slider.scrollLeft);
-  console.log(slider.offsetWidth);
-});
+// slider.addEventListener("scroll", (e) => {
+//   console.log(slider.scrollLeft);
+//   console.log(slider.offsetWidth);
+// });
